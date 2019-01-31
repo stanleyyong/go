6afde5c0c831db0199788ce3e0f209e12869849f
Stanley Yong <stanyong@sg.ibm.com>
2019-01-31 17:51:37 +0800

    This commits changes needed to the example to run successfully based on the XDR generated by the build_transaction sample to call World Wire's standalone test Stellar network

--- examples/submit_transaction/main.go
+++ examples/submit_transaction/main.go
@@ -3,13 +3,14 @@ package main
 import (
 	"fmt"
 
-	"github.com/stellar/go/clients/horizon"
+	"github.com/stanleyyong/go/clients/horizon"
 )
 
 func main() {
-	blob := "AAAAAH0t3PCq/ZCAvioV7ThK3edEcX3PcUrhoc0vngMsqGGWAAAAZAA1fpcAAAABAAAAAAAAAAAAAAABAAAAAAAAAAEAAAAANg+Af8x49GLeB3Un+Qr9ESJJAnsZmqTNpOqM9dTDnhkAAAAAAAAAAAAPQkAAAAAAAAAAASyoYZYAAABA/L7Du9hlYzCtR9F89Mp9/alkCXsq9CWuJ1Mpql+Q16fHE4P2+H62p4cx+b2YUp/fUX73ucW+RPxOgSXmeV6uBQ=="
+	//blob := "AAAAAH0t3PCq/ZCAvioV7ThK3edEcX3PcUrhoc0vngMsqGGWAAAAZAA1fpcAAAABAAAAAAAAAAAAAAABAAAAAAAAAAEAAAAANg+Af8x49GLeB3Un+Qr9ESJJAnsZmqTNpOqM9dTDnhkAAAAAAAAAAAAPQkAAAAAAAAAAASyoYZYAAABA/L7Du9hlYzCtR9F89Mp9/alkCXsq9CWuJ1Mpql+Q16fHE4P2+H62p4cx+b2YUp/fUX73ucW+RPxOgSXmeV6uBQ=="
+	blob := "AAAAAH0t3PCq/ZCAvioV7ThK3edEcX3PcUrhoc0vngMsqGGWAAAAZAAASp4AAAABAAAAAAAAAAAAAAABAAAAAAAAAAEAAAAANg+Af8x49GLeB3Un+Qr9ESJJAnsZmqTNpOqM9dTDnhkAAAAAAAAAAAHfUdwAAAAAAAAAASyoYZYAAABAtbe3Z2VfjBQbdkZhCgARL5LnHwY7vBgdiaFRSQWDKIZhm9zPAmjkNfiuf22h8hpHLNX5IRUfoGh0tjq6nzxsBg=="
 
-	resp, err := horizon.DefaultTestNetClient.SubmitTransaction(blob)
+	resp, err := horizon.DefaultWWNetClient.SubmitTransaction(blob)
 	if err != nil {
 		panic(err)
 	}
